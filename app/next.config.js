65dffea70235adadaccfd9a0ea343b5105ce5029
shibata-taiga <shibata56651@gmail.com>
2025-03-11 18:38:40 +0900

    fix: 処理対応

--- app/next.config.js
+++ app/next.config.js
@@ -2,11 +2,17 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
+  env: {
+    CHROME_PATH: process.env.CHROME_PATH || "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+  },
   experimental: {
     externalPackages: ["chrome-launcher", "lighthouse"],
   },
   output: "standalone",
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push("lighthouse");
+    }
     config.watchOptions = {
       poll: 1000,
       aggregateTimeout: 300,
--- init.sh
+++ init.sh
@@ -5,8 +5,13 @@ npm install --force
 cp .env.example ./.env
 cp .env.example ./app/.env
 
-docker network create a11y-lighthouse-tools
+docker network create a11y-tools
 
 docker compose build --no-cache
 
 docker compose up -d
+
+sleep 5
+
+CONTAINER_ID=$(docker ps -qf "name=a11y-tools-app")
+docker exec -it $CONTAINER_ID printenv | grep CHROME_PATH
